3a2aa0ed446697c6f01fad6476c2b847edd9b39f
lauren <lauren.a.fryman@gmail.com>
2020-08-30 23:14:08 -0400

    getting buttons to work

---
+++ ToDoList_08272020/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ToDoList_08272020.Models;
+
+namespace ToDoList_08272020.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserdbContext _userdb; 
+        public UserController(UserdbContext userdb)
+        {
+            _userdb = userdb; 
+        }
+        [Authorize]
+        public IActionResult Index()
+        {
+            var userToDoList = _userdb.ToDoList.ToList(); 
+
+            return View(userToDoList);
+        }
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public IActionResult AddTask(ToDoList newTask)
+        {
+            //assine new task userid to id, 
+            newTask.UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (ModelState.IsValid)
+            {
+                _userdb.ToDoList.Add(newTask);
+                _userdb.SaveChanges(); 
+            }
+            return RedirectToAction("ToDoList"); 
+        }
+        [Authorize]
+        public IActionResult ToDoList()
+        {
+            string id = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            List<ToDoList> toDoList = _userdb.ToDoList.Where(x => x.UserId == id).ToList();
+            return View(toDoList); 
+        }
+
+        public IActionResult DeleteTask(int Id)
+        {
+            var foundTask = _userdb.ToDoList.Find(Id);
+            if(foundTask != null)
+            {
+                _userdb.ToDoList.Remove(foundTask);
+                _userdb.SaveChanges(); 
+            }
+            return RedirectToAction("ToDoList"); 
+        }
+
+        public IActionResult MarkComplete(int Id)
+        {
+            var foundTask = _userdb.ToDoList.Find(Id);
+            if (foundTask != null)
+            {
+                foundTask.Complete = true;
+                _userdb.ToDoList.Update(foundTask);
+                _userdb.SaveChanges();
+            }
+            return RedirectToAction("ToDoList");
+        }
+    }
+}
---
+++ ToDoList_08272020/Models/ToDoList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList_08272020.Models
+{
+    public partial class ToDoList
+    {
+        public int Id { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Complete { get; set; }
+        public string UserId { get; set; }
+
+        public virtual AspNetUsers User { get; set; }
+    }
+}
